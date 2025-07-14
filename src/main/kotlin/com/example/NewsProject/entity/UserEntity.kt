9a1aa5fce1087b860fcff38e1e65f501ef60f17a
Тимофей <tl79266837738@gmail.com>
2025-07-14 17:56:02 +0300

    account publisher and user entities have been added

---
+++ src/main/kotlin/com/example/NewsProject/entity/UserEntity.kt
@@ -0,0 +1,18 @@
+package com.example.NewsProject.entity
+
+import com.example.NewsProject.consts.AccountTypes.USER
+import jakarta.persistence.Column
+import jakarta.persistence.DiscriminatorValue
+import jakarta.persistence.Entity
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.PrimaryKeyJoinColumn
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "user")
+@PrimaryKeyJoinColumn(referencedColumnName = "id",foreignKey = ForeignKey(name = "user_account_fk"))
+@DiscriminatorValue(USER)
+class UserEntity(): AccountEntity() {
+    @Column(name = "role")
+    var role: String? = null
+}
