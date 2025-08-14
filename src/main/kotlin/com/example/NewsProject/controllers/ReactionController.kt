1af5c0bb3f1aa8b91ac6e44523ebc629904ed0a1
hhheys <dmitriy.tsyganckov@gmail.com>
2025-08-14 11:15:50 +0300

    Added description for swagger docs

--- src/main/kotlin/com/example/NewsProject/controllers/ReactionController.kt
+++ src/main/kotlin/com/example/NewsProject/controllers/ReactionController.kt
@@ -6,6 +6,8 @@ import com.example.NewsProject.entity.AccountEntity
 import com.example.NewsProject.response.ReactionResponse
 import com.example.NewsProject.service.ReactionServiceImpl
 import com.example.NewsProject.service.kafka.KafkaService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.apache.coyote.BadRequestException
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -20,12 +22,14 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("/reaction")
+@Tag(name = "Reaction", description = "Логика взаимодействия с реакциями.")
 class ReactionController(
     private val reactionService: ReactionServiceImpl,
     private val kafkaService: KafkaService
 ) {
     @PostMapping("/add")
     @PreAuthorize("hasRole('ROLE_${AccountTypes.USER}')")
+    @Operation(summary = "Добавление реакции на пост.")
     fun addReactionPost(@RequestBody postReactionDto: PostReactionDto, @AuthenticationPrincipal accountDetails: AccountEntity){
         if (postReactionDto.type !in listOf("like","dislike")){
             throw BadRequestException("Reaction type not found")
@@ -37,6 +41,7 @@ class ReactionController(
     }
 
     @DeleteMapping("/remove/{uuid}")
+    @Operation(summary = "Удаление реакции с поста.")
     @PreAuthorize("hasAnyRole('ROLE_${AccountTypes.USER}', 'ROLE_${AccountTypes.PUBLISHER}')")
     fun removeReaction(@PathVariable uuid: UUID, @AuthenticationPrincipal accountDetails: AccountEntity){
         val accountUUID = accountDetails.id ?: throw BadRequestException()
@@ -44,6 +49,7 @@ class ReactionController(
     }
 
     @GetMapping("/post/{uuid}")
+    @Operation(summary = "Получение реакций по UUID поста.")
     fun getReactionsByPostID(@PathVariable uuid: UUID): MutableList<ReactionResponse> {
         return reactionService.findAllByPostId(uuid)
     }
--- src/main/kotlin/com/example/NewsProject/dto/PostUpdateDto.kt
+++ src/main/kotlin/com/example/NewsProject/dto/PostUpdateDto.kt
@@ -1,11 +1,16 @@
 package com.example.NewsProject.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.util.UUID
 
 data class PostUpdateDto (
+    @Schema(description = "Название поста")
     val title: String? = null,
+    @Schema(description = "Описание поста")
     val description: String? = null,
+    @Schema(description = "Основное содержимое поста")
     val content: String? = null,
+    @Schema(description = "Список ID топиков")
     val topicIDList: List<Int>? = null,
     val publisherUUID: UUID? = null
 )
